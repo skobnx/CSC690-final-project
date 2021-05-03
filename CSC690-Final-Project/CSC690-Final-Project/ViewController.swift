064c975f595b8a9e34cfbfe2aa67e0476b714e77
Sebastian Drake <skobnx@gmail.com>
2021-05-03 13:56:00 -0700

    inital commit

---
+++ CSC690-Final-Project/CSC690-Final-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CSC690-Final-Project
+//
+//  Created by Sebastian Drake on 5/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
