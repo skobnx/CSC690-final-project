39ae23d9f4ec010f7e9df1f9e6824eef90d87cd4
Sebastian Drake <skobnx@gmail.com>
2021-05-04 10:56:53 -0700

    When selecting a player, a type is now passed

--- CSC690-Final-Project/CSC690-Final-Project/GameViewController.swift
+++ CSC690-Final-Project/CSC690-Final-Project/GameViewController.swift
@@ -8,10 +8,14 @@
 import UIKit
 
 class GameViewController: UIViewController {
+    var player_type = 0
+    @IBOutlet weak var Character: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.navigationController?.isNavigationBarHidden = true
+        Character.text = String(player_type)
     }
 
 
--- CSC690-Final-Project/CSC690-Final-Project/PlayerSelectViewController.swift
+++ CSC690-Final-Project/CSC690-Final-Project/PlayerSelectViewController.swift
@@ -9,6 +9,22 @@ import UIKit
 
 class PlayerSelectViewController: UIViewController {
 
+    var player_type = 0
+    
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let gameViewController = segue.destination as? GameViewController else { return }
+            gameViewController.player_type = player_type
+    }
+    
+    @IBAction func SamuraiButton(_ sender: Any) {
+        self.player_type = 1
+    }
+    
+    @IBAction func MagicButton(_ sender: Any) {
+        self.player_type = 2
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
