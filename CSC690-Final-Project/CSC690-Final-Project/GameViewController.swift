6a2add192d544b508949f3edbc1062c3e5af7ccf
Sebastian Drake <skobnx@gmail.com>
2021-05-04 15:47:26 -0700

    Generating a random boss on game screen working

--- CSC690-Final-Project/CSC690-Final-Project/GameViewController.swift
+++ CSC690-Final-Project/CSC690-Final-Project/GameViewController.swift
@@ -9,15 +9,33 @@ import UIKit
 
 class GameViewController: UIViewController {
     var player_type = 0
+    var boss_type = 0
+    @IBOutlet weak var boss_image: UIImageView!
     @IBOutlet weak var Character: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.navigationController?.isNavigationBarHidden = true
-        Character.text = String(player_type)
+        if player_type == 1{
+            Character.text = "Samurai"
+        }else if player_type == 2{
+            Character.text = "Mage"
+        }
+        self.generateBoss()
+    }
+    
+    // function for generating a random boss to fight
+    func generateBoss(){
+        self.boss_type = Int.random(in: 1 ..< 3)
+        if self.boss_type == 1{
+            let dragon_image = #imageLiteral(resourceName: "dragon")
+            self.boss_image.image = dragon_image
+        } else if self.boss_type == 2{
+            let wolf_image = #imageLiteral(resourceName: "wolf")
+            self.boss_image.image = wolf_image
+        }
     }
 
 
 }
-
