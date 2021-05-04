d58b2b2786496e09559296dac20fe12d6543ec82
Sebastian Drake <skobnx@gmail.com>
2021-05-03 19:58:28 -0700

    Created main view and how to play view

---
+++ CSC690-Final-Project/CSC690-Final-Project/howToPlayViewController.swift
@@ -0,0 +1,19 @@
+//
+//  howToPlayViewController.swift
+//  CSC690-Final-Project
+//
+//  Created by Sebastian Drake on 5/3/21.
+//
+
+import UIKit
+
+class howToPlayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
