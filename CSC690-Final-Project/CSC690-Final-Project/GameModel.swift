f64c99876f66d2e5d0f688e55aa7b85cc660d37a
Sebastian Drake <skobnx@gmail.com>
2021-05-05 07:13:11 -0700

    Created model for game, and updated game VC to use model

---
+++ CSC690-Final-Project/CSC690-Final-Project/GameModel.swift
@@ -0,0 +1,86 @@
+//
+//  GameModel.swift
+//  CSC690-Final-Project
+//
+//  Created by Sebastian Drake on 5/4/21.
+//
+import UIKit
+import Foundation
+
+class GameModel {
+    var player_type = 0
+    var boss_type = 0
+    var player_image: UIImage? = nil
+    var boss_image: UIImage? = nil
+    
+    // Hold the types of buttons
+    var button_one_type = 0
+    var button_two_type = 0
+    var button_three_type = 0
+    var button_one_image: UIImage? = nil
+    var button_two_image: UIImage? = nil
+    var button_three_image: UIImage? = nil
+    
+    // function for generating a random boss to fight
+    func generateBoss(){
+        self.boss_type = Int.random(in: 1 ..< 3)
+        if self.boss_type == 1{
+            self.boss_type = 1
+            let dragon_image = #imageLiteral(resourceName: "dragon")
+            self.boss_image = dragon_image
+        } else if self.boss_type == 2{
+            self.boss_type = 2
+            let wolf_image = #imageLiteral(resourceName: "wolf")
+            self.boss_image = wolf_image
+        }
+        NotificationCenter.default.post(name: Notification.Name("boss_created"), object: nil)
+    }
+    
+    // function for displaying the player image
+    func generatePlayer(playerType: Int){
+        self.player_type = playerType
+        if self.player_type == 1{
+            let playerImage = #imageLiteral(resourceName: "player_samurai")
+            self.player_image = playerImage
+        } else if self.player_type == 2{
+            let playerImage = #imageLiteral(resourceName: "player_magic")
+            self.player_image = playerImage
+        }
+        NotificationCenter.default.post(name: Notification.Name("player_created"), object: nil)
+    }
+    
+    func generateButtons(){
+        var array = [1, 2, 3, 4, 5]
+        array.shuffle()
+        let card1 = array[0]
+        let card2 = array[1]
+        let card3 = array[2]
+        self.button_one_image = self.getButtonImage(num: card1)
+        self.button_two_image = self.getButtonImage(num: card2)
+        self.button_three_image = self.getButtonImage(num: card3)
+        NotificationCenter.default.post(name: Notification.Name("buttons_updated"), object: nil)
+    }
+    
+    func getButtonImage(num: Int) -> UIImage{
+        switch num {
+        case 1:
+            //return attack
+            return #imageLiteral(resourceName: "attack_card")
+        case 2:
+            //return defend
+            return #imageLiteral(resourceName: "defend_card")
+        case 3:
+            //return heal
+            return #imageLiteral(resourceName: "heal_card")
+        case 4:
+            //return special
+            return #imageLiteral(resourceName: "special_card")
+        case 5:
+            //return prepare
+            return #imageLiteral(resourceName: "prepare_card")
+        default:
+            return #imageLiteral(resourceName: "empty_button")
+        }
+    }
+    
+}
--- CSC690-Final-Project/CSC690-Final-Project/GameViewController.swift
+++ CSC690-Final-Project/CSC690-Final-Project/GameViewController.swift
@@ -16,6 +16,8 @@ class GameViewController: UIViewController {
     var button_two_type = 0
     var button_three_type = 0
     
+    let model = GameModel()
+    
     @IBOutlet weak var boss_image: UIImageView!
     @IBOutlet weak var player_image: UIImageView!
     @IBOutlet weak var Character: UILabel!
@@ -27,75 +29,33 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.navigationController?.isNavigationBarHidden = true
-        if player_type == 1{
+
+        NotificationCenter.default.addObserver(self, selector: #selector(updateBoss), name: Notification.Name("boss_created"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(updatePlayer), name: Notification.Name("player_created"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(updateButtons), name: Notification.Name("buttons_updated"), object: nil)
+        
+        model.generatePlayer(playerType: self.player_type)
+        model.generateBoss()
+        model.generateButtons()
+    }
+    
+    @objc func updateBoss(){
+        self.boss_type = model.boss_type
+        self.boss_image.image = model.boss_image
+    }
+    
+    @objc func updatePlayer(){
+        self.player_image.image = model.player_image
+        if self.player_type == 1{
             Character.text = "Samurai"
-        } else if player_type == 2{
+        } else if self.player_type == 2{
             Character.text = "Mage"
         }
-        self.generatePlayer()
-        self.generateBoss()
-        self.generateButtons()
     }
-    
-    // function for generating a random boss to fight
-    func generateBoss(){
-        self.boss_type = Int.random(in: 1 ..< 3)
-        if self.boss_type == 1{
-            self.boss_type = 1
-            let dragon_image = #imageLiteral(resourceName: "dragon")
-            self.boss_image.image = dragon_image
-        } else if self.boss_type == 2{
-            self.boss_type = 2
-            let wolf_image = #imageLiteral(resourceName: "wolf")
-            self.boss_image.image = wolf_image
-        }
-    }
-    
-    // function for displaying the player image
-    func generatePlayer(){
-        if self.player_type == 1{
-            let playerImage = #imageLiteral(resourceName: "player_samurai")
-            self.player_image.image = playerImage
-        } else if self.player_type == 2{
-            let playerImage = #imageLiteral(resourceName: "player_magic")
-            self.player_image.image = playerImage
-        }
-    }
-
-    func generateButtons(){
-        var array = [1, 2, 3, 4, 5]
-        array.shuffle()
-        let card1 = array[0]
-        let card2 = array[1]
-        let card3 = array[2]
-        let button_one_image = self.getButtonImage(num: card1)
-        let button_two_image = self.getButtonImage(num: card2)
-        let button_three_image = self.getButtonImage(num: card3)
-        self.button_one.setImage(button_one_image, for: UIControl.State.normal)
-        self.button_two.setImage(button_two_image, for: UIControl.State.normal)
-        self.button_three.setImage(button_three_image, for: UIControl.State.normal)
-    }
-    
-    func getButtonImage(num: Int) -> UIImage{
-        switch num {
-        case 1:
-            //return attack
-            return #imageLiteral(resourceName: "attack_card")
-        case 2:
-            //return defend
-            return #imageLiteral(resourceName: "defend_card")
-        case 3:
-            //return heal
-            return #imageLiteral(resourceName: "heal_card")
-        case 4:
-            //return special
-            return #imageLiteral(resourceName: "special_card")
-        case 5:
-            //return prepare
-            return #imageLiteral(resourceName: "prepare_card")
-        default:
-            return #imageLiteral(resourceName: "empty_button")
-        }
+    @objc func updateButtons(){
+        self.button_one.setImage(model.button_one_image, for: UIControl.State.normal)
+        self.button_two.setImage(model.button_two_image, for: UIControl.State.normal)
+        self.button_three.setImage(model.button_three_image, for: UIControl.State.normal)
     }
 
 }
